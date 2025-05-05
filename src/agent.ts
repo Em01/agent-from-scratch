b16b5e848b7415e97c963f6c58758a9df846369c
Emma Williams <github@em01.co.uk>
2025-05-05 20:13:01 +0100

    fn calling

--- src/agent.ts
+++ src/agent.ts
@@ -1,8 +1,8 @@
 import type { AIMessage } from '../types'
-import { addMessages, getMessages } from './memory'
+import { addMessages, getMessages, saveToolResponse } from './memory'
 import { runLLM } from './llm'
 import { showLoader, logMessage } from './ui'
-
+import { runTool } from './toolRunner'
 export const runAgent = async ({
   userMessage,
   tools,
@@ -16,14 +16,18 @@ export const runAgent = async ({
   const history = await getMessages()
 
   const response = await runLLM({ messages: history, tools })
-
-  if (response.tool_calls) {
-    console.log(response.tool_calls)
-  }
-
   await addMessages([response])
 
-  // logMessage(response)
+  if (response.tool_calls) {
+    const toolCall = response.tool_calls[0]
+    loader.update(`done: ${toolCall.function.name}`)
+
+    const toolResponse = await runTool(toolCall, userMessage)
+    await saveToolResponse(toolCall.id, toolResponse)
+    loader.update(`done: ${toolCall.function.name}`)
+  }
+
+  logMessage(response)
   loader.stop()
   return getMessages()
 }
